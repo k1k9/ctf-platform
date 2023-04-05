5deefca2d19857d048e5353cdbd9d1e2c3426ba0
Krystian <k1k9@protonmail.com>
2023-04-05 12:59:20 +0200

    Changed hardcoded credentials into based on env variables

--- api/database.py
+++ api/database.py
@@ -1,8 +1,16 @@
+from os import environ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-url_database = "mysql+mysqldb://root:""@localhost:3306/test3"
+# Setting up connection based on env vars
+host = environ.get('DATABASE_HOST')
+user = environ.get('DATABASE_USER')
+password = environ.get('DATABASE_PASSWORD') if environ.get('DATABASE_PASSWORD') != None else ""
+port = environ.get('DATABASE_PORT')
+name = environ.get('DATABASE_NAME')
+
+url_database = f"mysql+mysqldb://{user}:{password}@{host}:{port}/{name}"
 
 engine = create_engine(
     url_database
